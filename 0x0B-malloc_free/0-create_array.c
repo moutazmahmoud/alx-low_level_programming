6fe4cc29f426054d7d400117a95a1edc2741e5f4
moutazmahmoud <moutaz.prof.egy@gmail.com>
2023-11-21 16:05:40 +0200

    refactor: betty 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,24 +22,22 @@ char *create_array(unsigned int size, char c)
 		
 {
 		
-	char *str;
+char *str;
 		
-	unsigned int i;
+unsigned int i;
 		
 
 		
-	str = malloc(sizeof(char) * size);
+str = malloc(sizeof(char) * size);
 		
-	if (size == 0 || str == NULL)
+if (size == 0 || str == NULL)
 		
 		return (NULL);
 		
 
 		
-	for (i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 		
-		str[i] = c;
+str[i] = c;
 		
-	return (str);
-		
-}
+return (str);}
