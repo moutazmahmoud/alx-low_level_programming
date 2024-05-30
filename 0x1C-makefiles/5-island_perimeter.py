f49a1262f62131a124ce1b408d40b6ab2e66a3c8
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-05-30 04:04:41 +0300

    Refactor: makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,7 +3,7 @@
 
 
 def island_perimeter(grid):
-    """Return the perimeter of an island.
+    """Return the perimiter of an island.
 
     The grid represents water by 0 and land by 1.
 
@@ -12,20 +12,17 @@ def island_perimeter(grid):
     Returns:
         The perimeter of the island defined in grid.
     """
-    height = len(grid)
     width = len(grid[0])
-    perimeter = 0
+    height = len(grid)
+    edges = 0
+    size = 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                # Add 4 for the current land cell
-                perimeter += 4
-                
-                # Subtract 2 for each adjacent land cell
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
